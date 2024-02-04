86f27658e468cf0dfd52ac909d9783cb23d8c17e
Tomas <103144015+Batoru1@users.noreply.github.com>
2024-02-04 15:35:38 +0200

    starting 2

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-//1 code to display who/how many LIKED
+//1 ou probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 
 function likes(names) {
   if (names.length === 0) {
@@ -21,4 +29,16 @@ console.log(likes(['Max', 'John', 'Mark']));
 console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']));
 console.log(likes(['Alex', 'Jacob', 'Mark', 'Max', 'hjkhjk']));
 
-//2
+//2 Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples (input --> output):
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "###################################
